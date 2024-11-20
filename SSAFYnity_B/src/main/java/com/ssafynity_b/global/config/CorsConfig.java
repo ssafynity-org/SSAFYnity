073da2a43400f868e3be67c05c5c2a38f437e320
leewoo <dldnwls009@gmail.com>
2024-11-20 20:25:24 +0900

    ADD: 개발환경, 배포환경 파일 생성 / @Value로 환경 구분

--- SSAFYnity_B/src/main/java/com/ssafynity_b/global/config/CorsConfig.java
+++ SSAFYnity_B/src/main/java/com/ssafynity_b/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ssafynity_b.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,10 +10,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    //Jenkins설정한 Cors값 주입
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000"); // React 서버
+        configuration.addAllowedOrigin(allowedOrigins); // React 서버
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 인증 정보 허용
--- SSAFYnity_B/src/main/java/com/ssafynity_b/global/config/WebConfig.java
+++ SSAFYnity_B/src/main/java/com/ssafynity_b/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ssafynity_b.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    //Jenkins설정한 Cors값 주입
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
