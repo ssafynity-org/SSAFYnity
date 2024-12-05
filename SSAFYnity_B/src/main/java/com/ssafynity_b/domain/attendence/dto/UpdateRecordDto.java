23229e4e1e948609dd63c88216e91371cfb1b23c
leewoo <dldnwls009@gmail.com>
2024-12-05 12:12:54 +0900

    Fix:[BE] 출결관련API(attendance API)에 UserDetails 적용

--- SSAFYnity_B/src/main/java/com/ssafynity_b/domain/attendence/dto/UpdateRecordDto.java
+++ SSAFYnity_B/src/main/java/com/ssafynity_b/domain/attendence/dto/UpdateRecordDto.java
@@ -9,10 +9,17 @@ import lombok.*;
 @AllArgsConstructor
 public class UpdateRecordDto {
 
-    private String id;
+    //수정할 년도
+    private int year;
+    //수정할 달
+    private int month;
+    //수정할 날
     private int day;
+    //체크인 시간
     private String checkInTime;
+    //체크아웃 시간
     private String checkOutTime;
+    //상태
     private String status;
 
 }
