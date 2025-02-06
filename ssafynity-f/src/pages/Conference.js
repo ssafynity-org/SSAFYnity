5a31a8df83c818e95e3656cc19a5b8b73212c666
박혜선 (Hyeseon Park) <qkr8364@naver.com>
2025-02-06 16:17:25 +0900

    FEAT: 컨퍼런스 페이지 왼쪽 네비게이션바 생성

---
+++ ssafynity-f/src/pages/Conference.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../api/axiosInstance";
+import { useNavigate } from "react-router-dom"; // useNavigate import
+import SearchBar from "../components/SearchBar";
+import "../styles/Conference.css";
+
+function Conference() {
+
+  const [menu, setMenu] = useState("전체 글");
+
+  const handleMenu = (menu) => {
+    setMenu(menu);
+    if(menu === "기업 별"){
+      setCompany("")
+    }
+  }
+
+  const [company, setCompany] = useState("");
+  const handleCompany = (company) => {
+    setCompany(company);
+  }
+  
+  return (
+    <div className="conference-page">
+      <SearchBar selectedMenu={handleMenu} selectedCompany={handleCompany} />
+      <h1>{menu}</h1>
+      {menu === "기업 별" && (
+        <h2>{company}</h2>
+      )}    
+    </div>
+  );
+}
+
+export default Conference;
