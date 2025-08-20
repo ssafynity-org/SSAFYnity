fef9ae3002092eaa2530490acc4556fdde660c97
leewoo <dldnwls009@gmail.com>
2025-08-21 01:55:25 +0900

    [25.08.21] 외부컨퍼런스 아이콘 고민중

--- ssafynity-f/src/components/ConferenceHeader.js
+++ ssafynity-f/src/components/ConferenceHeader.js
@@ -1,16 +1,18 @@
 import "../styles/ConferenceHeader.css";
 
-function conferenceHeader() {
-
+function ConferenceHeader() {
     return (
         <div className="conferenceHeader">
-            <img src="/images/bigLogo.png" alt="Logo"/>
+            <img 
+                src="/images/tech-folder.png" 
+                alt="Logo" 
+                style={{ width: "100px", height: "auto" }}
+            />
             <div className="conferenceHeader-discription">
-                <h1>다양한 기술 컨퍼런스를 확인해보세요</h1>
-                <p>성장을 위한 공간입니다.</p>
+                <h1>Conference</h1>
             </div>
         </div>
     )
 }
 
-export default conferenceHeader;
+export default ConferenceHeader;
